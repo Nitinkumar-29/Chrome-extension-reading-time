b13405f21c6d54172e49f9e50c0f80f8fa6fa8e8
nitin <nimble2405@gmail.com>
2024-07-11 11:56:09 +0530

    can article and how many it will take to read an article, currently supported only with chrome docs article

---
+++ scripts/content.js
@@ -0,0 +1,18 @@
+const article = document.querySelector("article");
+
+if (article) {
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g;
+  const words = text.matchAll(wordMatchRegExp);
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  //   support for api reference docs
+  const heading = article.querySelector("h1");
+  // support for articels docs with date
+  const date = article.querySelector("time")?.parentNode;
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
